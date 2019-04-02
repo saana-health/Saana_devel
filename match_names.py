d8ee2370da67d9282d48eacfdb881cba69f7a926
minjoon <somj94@gmail.com>
2019-04-02 04:02:09 -0400

    frozen garden process done

--- match_names.py
+++ match_names.py
@@ -18,7 +18,7 @@ def match_euphebe():
     # combined = processEuphebe.process()
 
     # remove matched from this list
-    euphebe_tag_no_match = euphebe_all[:]
+    tag_no_match = euphebe_all[:]
 
     # dictionary to be used to convert in 'processEuphebe()'
     convert_dic = {'pot':'potassium', 'sod': 'sodium', 'yukon gold potatoes': 'potato'}
@@ -33,8 +33,8 @@ def match_euphebe():
             if tag_keyword in euphebe_name:
                 convert_dic[euphebe_name] = tag_keyword
                 print('{}  |  {}'.format(tag_keyword, euphebe_name))
-                if euphebe_name in euphebe_tag_no_match:
-                    euphebe_tag_no_match.remove(euphebe_name)
+                if euphebe_name in tag_no_match:
+                    tag_no_match.remove(euphebe_name)
                 # if tag_keyword in tag_no_match:
                 #     tag_no_match.remove(tag_keyword)
 
@@ -47,22 +47,22 @@ def match_euphebe():
     for tag_keyword in tag_no_match[:]:
         if tag_keyword not in keyword_dictionary.keys():
             continue
-        for euphebe_name in euphebe_tag_no_match:
+        for euphebe_name in tag_no_match:
             val = keyword_dictionary[tag_keyword]
             if isinstance(val,(list,)):
                 for tag_keyword_ in val:
                     if tag_keyword_ in euphebe_name:
                         convert_dic[euphebe_name] = tag_keyword
                         print('{}  |  {}'.format(tag_keyword_, euphebe_name))
-                        if euphebe_name in euphebe_tag_no_match:
-                            euphebe_tag_no_match.remove(euphebe_name)
+                        if euphebe_name in tag_no_match:
+                            tag_no_match.remove(euphebe_name)
                         if tag_keyword in tag_no_match:
                             tag_no_match.remove(tag_keyword)
             else:
                 if val in euphebe_name:
                     convert_dic[euphebe_name] = tag_keyword
-                    if euphebe_name in euphebe_tag_no_match:
-                        euphebe_tag_no_match.remove(euphebe_name)
+                    if euphebe_name in tag_no_match:
+                        tag_no_match.remove(euphebe_name)
                     print('{}  |  {}'.format(tag_keyword, euphebe_name))
                     if tag_keyword in tag_no_match:
                         tag_no_match.remove(tag_keyword)
@@ -98,8 +98,65 @@ def match_foodnerd():
 
     pickle.dump(convert_dic,open('foodnerd_change.p','wb'))
 
+def match_frozenGarden():
+    import processFrozenGarden
+    PATH = os.path.join(os.getcwd(),'csv/frozenGarden/')
+    for filename in os.listdir(PATH):
+        if filename[0] != '.':
+            full_list = processFrozenGarden.processNutrition(PATH + filename)
+    _, matrix_columns = processFoodMatrix.processFoodMatrixCSV('foodtag313.csv')
 
+    tag_no_match = matrix_columns[:]
+    euphebe_no_match = full_list[:]
+    convert_dic = {}
+    for each in convert_dic.values():
+        matrix_columns.remove(each)
+
+    tag_no_match = matrix_columns[:]
+
+    for tag_keyword in matrix_columns:
+        print('--------')
+        for euphebe_name in full_list:
+            if tag_keyword in euphebe_name:
+                convert_dic[euphebe_name] = tag_keyword
+                print('{}  |  {}'.format(tag_keyword, euphebe_name))
+                if euphebe_name in tag_no_match:
+                    tag_no_match.remove(euphebe_name)
+                # if tag_keyword in tag_no_match:
+                #     tag_no_match.remove(tag_keyword)
+
+    #for Euphebe - keys are for Euphebe and values for food tags matrix
+    keyword_dictionary = {'nut, nut butter':'nuts','brussels sprouts': 'brussel sprouts','vitamin k': 'vit k', 'insoluble fiber': 'totinfib','total fiber': 'totfib','turmeric (curcumin)': 'turmeric',\
+                          'peppers, bell': ['pepper, bell','bell pepper'], 'peppers, hot':['pepper, hot','jalapeno pepper','chili pepper','poblano pepper','serrano pepper'], 'spicy powders':\
+                              ['black pepper','curry, powder']}
+
+    print('------semi manual--------')
+    for tag_keyword in tag_no_match[:]:
+        if tag_keyword not in keyword_dictionary.keys():
+            continue
+        for euphebe_name in tag_no_match:
+            val = keyword_dictionary[tag_keyword]
+            if isinstance(val,(list,)):
+                for tag_keyword_ in val:
+                    if tag_keyword_ in euphebe_name:
+                        convert_dic[euphebe_name] = tag_keyword
+                        print('{}  |  {}'.format(tag_keyword_, euphebe_name))
+                        if euphebe_name in tag_no_match:
+                            tag_no_match.remove(euphebe_name)
+                        if tag_keyword in tag_no_match:
+                            tag_no_match.remove(tag_keyword)
+            else:
+                if val in euphebe_name:
+                    convert_dic[euphebe_name] = tag_keyword
+                    if euphebe_name in tag_no_match:
+                        tag_no_match.remove(euphebe_name)
+                    print('{}  |  {}'.format(tag_keyword, euphebe_name))
+                    if tag_keyword in tag_no_match:
+                        tag_no_match.remove(tag_keyword)
+    pdb.set_trace()
+    # pickle.dump(convert_dic,open('euphebe_change_'+DATE+'.p','wb'))
 
 if __name__ == "__main__":
     # match_foodnerd()
-    match_euphebe()
+    # match_euphebe()
+    match_frozenGarden()
--- processFrozenGarden.py
+++ processFrozenGarden.py
@@ -6,7 +6,7 @@ import os
 from model import Meal
 from difflib import SequenceMatcher
 from connectMongdo import add_meals
-from utils import unicodetoascii
+# from utils import unicodetoascii
 import re
 
 PATH = os.path.join(os.getcwd(),'csv/frozenGarden/')
@@ -26,20 +26,32 @@ def processNutrition(filename):
     with open(filename) as csvfile:
         ingredients = {}
         nutritions = {}
-        all_contents = []
 
         reader_list = list(csv.reader(csvfile))
+        meal_name = reader_list[1][0]
         columns = [x.strip().lower() for x in reader_list[3]]
-        for each in columns:
-            print(unicodetoascii(each))
+        # for each in columns:
+        #     # print(each.encode('utf-8').decode('unicode_escape'))
+        #     # print(each.decode().encode('utf-8'))
+        #     print(each.encode('utf-8'))
         # columns = [x.strip().lower().replace('\\xc2\\xac\\xc2\\xb','n') if '\\xc2\\xac\\xc2\\xb' in x else x.strip().lower() for x in list(reader_list[3])]
 
-        pdb.set_trace()
 
         units = [re.search('\(.{0,3}\)',columns[y]).group()[1:-1] if re.search('\(.{0,3}\)',columns[y]) else '' for y in range(len(columns))]
         for i in range(len(columns)):
             name = columns[i].replace(' ('+units[i]+')','')
             columns[i] = name
+        # test = columns[-5][10]
+
+        for row in range(4,len(reader_list)-2):
+            ingredients[reader_list[row][0].strip().lower()] = reader_list[row][1]
+        for ind in range(len(units)):
+            if units[ind] == '':
+                continue
+            nutritions[columns[ind]] = reader_list[-2][ind] + ' ' + units[ind]
+        new_meal = Meal(name = meal_name, ingredients=ingredients, nutrition= nutritions, supplierID='FrozenGarden')
+        return list(ingredients.keys()) + list(set(columns))
+        pdb.set_trace
 
 
 if __name__ == "__main__":
