332bce7e18987d9f4f789c22b93ffe17665171d5
minjoon <somj94@gmail.com>
2019-04-04 12:11:48 -0400

    sendgrid

---
+++ send_email.py
@@ -0,0 +1,37 @@
+import os
+import json
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+to_emails = [
+    # ('somj94@gmail.com', 'Min Joon')
+    # ,
+    ('minjoon@saana.co', 'minjoon')
+]
+
+to_name = 'first-name'
+
+message = Mail(
+    from_email=('minjoon@saana.co', 'Min Joon'),
+    to_emails=to_emails,
+    subject='Your Saana Order is Confirmed!',
+    html_content='Hi <strong>' + to_name + "</strong>, \
+    <br> <h1> Your personalize nutrition has just been calculated.</h1> \
+    <p style = 'widht:200 px;'>Good News! Our team of scientists just finished up personalizing your next delivery of meals based on your latest medical information. \
+    <br>Our suppliers will start cooking your meals next Monday and you should receive it by the end of next week (shipped within 1-2 business days). \
+    <br>Your next tailored meal program, that will therefore start next Friday, will be sent over in a separate email.</p>\
+    <br> <p style = 'width: 70%'>As always, don’t hesitate to contact us via text message (862) 256-3059 or email (hello@saana.co) for any questions regarding your meals,\
+     your well-being or anything! Coaching and follow-up is always part of your plan.</p>\
+    <br> With love,\
+    <br><br> Saana Team"
+)
+
+
+try:
+    sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sendgrid_client.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
