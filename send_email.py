ef69070516a9a49136683ce95ac2747537ee1a5c
Min Joon So <somj94@gmail.com>
2019-04-05 12:16:26 -0400

    email format from Shubham

--- send_email.py
+++ send_email.py
@@ -1,35 +1,22 @@
 import os
+import pdb
 import json
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 to_emails = [
-    # ('somj94@gmail.com', 'Min Joon')
-    # # ,
-    ('stephanie@saana.co', 'minjoon')
+    # ('somj94@gmail.com', 'Min Joon'),
+    ('stephanie@saana.co','Stephanie')
 ]
 
-to_name = 'first-name'
+to_name = 'Beata'
 
 message = Mail(
-    from_email=('minjoon@saana.co', 'Min Joon'),
+    from_email=('hello@saana.co', 'Jacki from Saana'),
     to_emails=to_emails,
     subject='Your Saana Order is Confirmed',
-    html_content='Hi <strong>' + to_name + "\
-    </strong>, \
-    <br> <h1> Your personalize nutrition has just been calculated.</h1> \
-    <p style = \"width\":60%;'>Good News! Our team of scientists just finished up personalizing your next delivery of meals based on your latest medical information. \
-    <br>Our suppliers will start cooking your meals next Monday and you should receive it by the end of next week (shipped within 1-2 business days). \
-    <br>Your next tailored meal program, that will therefore start next Friday, will be sent over in a separate email.</p>\
-    <p style = \"width: 60%\">As always, don’t hesitate to contact us via text message (862) 256-3059 or email (hello@saana.co) for any questions regarding your meals,\
-     your well-being or anything! Coaching and follow-up is always part of your plan.</p>\
-    With love,\
-    <br> Saana Team\
-    <br>\
-    <img src='https://minjoon-test-bucket.s3.amazonaws.com/logo', height = \"40\", width = \"160\">\
-    "
+    html_content = open('email_format.html','r').read()
 )
-
 
 try:
     sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
