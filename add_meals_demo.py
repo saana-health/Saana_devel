cfbd8a96053d78e35dedd65d922e083499e2ca3e
stephanieroland <stephanie.roland89@gmail.com>
2019-09-04 18:21:49 -0400

    adapting add-meals to colruyt

--- add_meals_demo.py
+++ add_meals_demo.py
@@ -1,14 +1,14 @@
-from saana_lib.process import processOisix, processEuphebe, processFrozenGarden, processFoodNerd, processVeestro, processFoodFlo, processFoodMatrix, match_names
+from saana_lib.process import processColruyt, processOisix, processEuphebe, processFrozenGarden, processFoodNerd, processVeestro, processFoodFlo, processFoodMatrix, match_names
 import os
 from saana_lib import connectMongo, model
 
 
 def add_meals():
     PATH = os.getcwd()
-    print('Adding Oisix meals')
-    ingredient_meals, ingredient_list = processOisix.processIngredients(PATH+'/csv/Oisix/demo_ingredients_full.csv')
-    nutrition_meals, nutrition_list = processOisix.processNutrition(PATH+'/csv/Oisix/demo_nutrition_full.csv')
-    combined = processOisix.combine_mealinfo(ingredient_meals, nutrition_meals)
+    print('Adding Colruyt meals')
+    ingredient_meals, ingredient_list = processColruyt.processIngredients(PATH+'/csv/Colruyt/demo_ingredients.csv')
+    nutrition_meals, nutrition_list = processColruyt.processNutrition(PATH+'/csv/Colruyt/demo_nutrition.csv')
+    combined = processColruyt.combine_mealinfo(ingredient_meals, nutrition_meals)
     full_list = ingredient_list + nutrition_list
     convert_dic = match_names.match_euphebe(full_list)
     combined2 = match_names.change_names(combined,convert_dic)
